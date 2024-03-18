bd60f72f0e7fb9fcab2723daa8e01fde8434b936
Janis <janis@github.com>
2024-03-18 18:25:35 +0100

    move the swift protocols to objc header

--- Sources/Pinnacle/Hooks/SBFloatingDockControllerHook.x.swift
+++ Sources/Pinnacle/Hooks/SBFloatingDockControllerHook.x.swift
@@ -1,35 +1,18 @@
 import Orion
 import PinnacleC
 
-@objc protocol SBHomeScreenViewController {
-  var homeScreenFloatingDockAssertion: SBFloatingDockBehaviorAssertion? { get }
-}
-
-@objc  protocol SBFloatingDockBehaviorAssertion {
-  var floatingDockController: SBFloatingDockController? { get }
-}
-
-@objc protocol SBFloatingDockController {
-  func _dismissFloatingDockIfPresentedAnimated(_ animated: Bool, completionHandler: (() -> Void)?)
-  func _presentFloatingDockIfDismissedAnimated(_ animated: Bool, completionHandler: (() -> Void)?)
-}
-
-@objc protocol IconController {
-  var parentViewController: SBHomeScreenViewController { get }
-}
-
 struct Dock {
   static func hideDock() {
     guard UIDevice.current.userInterfaceIdiom == .pad else { return }
 
-    let iconController = SBIconController.sharedInstance().as(interface: IconController.self)
-    iconController.parentViewController.homeScreenFloatingDockAssertion?.floatingDockController?._dismissFloatingDockIfPresentedAnimated(true, completionHandler: nil)
+    let iconController = SBIconController.sharedInstance().as(interface: SBIconController.self)
+    iconController.parentViewController.homeScreenFloatingDockAssertion?.floatingDockController?._dismissFloatingDockIfPresented(animated: true, completionHandler: nil)
   }
 
   static func showDock() {
     guard UIDevice.current.userInterfaceIdiom == .pad else { return }
-    
-    let iconController = SBIconController.sharedInstance().as(interface: IconController.self)
-    iconController.parentViewController.homeScreenFloatingDockAssertion?.floatingDockController?._presentFloatingDockIfDismissedAnimated(true, completionHandler: nil)
+
+    let iconController = SBIconController.sharedInstance().as(interface: SBIconController.self)
+    iconController.parentViewController.homeScreenFloatingDockAssertion?.floatingDockController?._presentFloatingDockIfDismissed(animated: true, completionHandler: nil)
   }
 }
